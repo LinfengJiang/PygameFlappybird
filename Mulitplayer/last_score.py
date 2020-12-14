8dfe9326d16b34251ca01b60524143c68424eb63
Linfeng <zeKr0@hotmail.com>
2020-12-15 06:08:03 +0800

    register funktion

--- Mulitplayer/last_score.py
+++ Mulitplayer/last_score.py
@@ -1,10 +1,14 @@
 import Mulitplayer.IDgenerate as idgen
 import Mulitplayer.database as db
+import Mulitplayer.register as reg
 
 #input the score. and use diskID for ID
 def insert_last_score(score):
     id = idgen.codegen()
-    sql_command = 'UPDATE `pygame`.`flappybird` SET `LAST_SCORE` = \'%s\' WHERE `flappybird`.`ID` = \'%s\'' % (score,id)
+    try:
+        sql_command = 'UPDATE `pygame`.`flappybird` SET `LAST_SCORE` = \'%s\' WHERE `flappybird`.`ID` = \'%s\'' % (score,id)
+    except:
+        reg.register(name='test')
     print(sql_command)
     db.sql_exec(sql_command)
 
